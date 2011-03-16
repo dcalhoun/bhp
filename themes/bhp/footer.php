0d44071a87a51921aba43f553efa2df8b0b4c915
David Calhoun <dcalhoun@centresource.com>
2011-03-15 23:43:28 -0500

    Updated footer.

--- themes/bhp/footer.php
+++ themes/bhp/footer.php
@@ -1,6 +1,6 @@
   <div class="grid_1">&nbsp;</div>
   <footer role="site" class="grid_11">
-    <p>&copy; Copyright <?php echo date('Y'); ?>. <?php bloginfo('site_title'); ?>. All Rights Reserved. Site by <a href="http://davidcalhoundesign.com" title="David Calhoun Design">David Calhoun</a>.</p>
+    <p>&copy; Copyright <?php echo date('Y'); ?> <?php bloginfo('site_title'); ?>. All Rights Reserved. Site by <a href="http://davidcalhoundesign.com" title="David Calhoun Design">David Calhoun</a>.</p>
   </footer>
   
 </div> <!-- #wrapper -->
