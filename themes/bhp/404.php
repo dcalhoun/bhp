fdece40394f2d8b168cf7180680e60c347dd4fcb
David Calhoun <dcalhoun@centresource.com>
2011-03-16 21:17:47 -0500

    Added 404 page.

---
+++ themes/bhp/404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<div class="grid_1">&nbsp;</div>
+<div id="page" class="grid_11">
+  <p class="textcenter no-posts">Sorry, I lost that page...</p>
+</div>
+<?php get_footer(); ?>
