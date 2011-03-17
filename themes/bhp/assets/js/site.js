f043dba1c20d26697a6f53cf4b3dfda4a277be6e
David Calhoun <dcalhoun@centresource.com>
2011-03-16 21:01:24 -0500

    Fixed sliding gallery titles on hover.

--- themes/bhp/assets/js/site.js
+++ themes/bhp/assets/js/site.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 
-  // Highlight 'photos' navigation item when on a gallery page
+  // Highlight "photos" navigation item when on a gallery page
   if ( $('body').hasClass('post-type-archive-gallery') || $('body').hasClass('tax-style') || $('body').hasClass('single-gallery') ) {
     var current_page = $('nav#primary-navigation ul li.current_page_parent');
     var gallery_page = $('nav#primary-navigation ul li a[href="/gallery"]');
@@ -9,15 +9,10 @@ jQuery(document).ready(function($) {
   }
   
   // Slide gallery titles in on hover
-  /*$('div.photo').hover(function(){
-    $(this).find('span.label').animate({bottom:'0'},{queue:false,duration:500});
-    }, function() {
-    };
-  });
   $('div.photo').hover(function() {
-    $(this).find('span.label').animate({bottom:'0'},{queue:false,duration:500});
+    $(this).find('span.label').animate({bottom:'0'},{queue:false,duration:400});
     }, function(){
-      $(this).find('span.label').animate({bottom:'-44px'},{queue:false,duration:500});
-  });*/
+      $(this).find('span.label').animate({bottom:'-44px'},{queue:false,duration:400});
+  });
   
 });
