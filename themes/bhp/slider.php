4bf9b2f81d0571bc06f7eb175486038ad38fa44d
David Calhoun <dcalhoun@centresource.com>
2011-04-17 17:21:21 -0500

    Updated social links to open new window.

--- themes/bhp/slider.php
+++ themes/bhp/slider.php
@@ -23,9 +23,9 @@ get_header(); ?>
   </div>
   <nav id="social-links" class="grid_3 omega">
     <ul>
-      <li><a href="http://twitter.com/ha1eyrobin" title="Follow me on Twitter" class="twitter">Twitter</a></li>
-      <li><a href="http://www.facebook.com/photosbyheart" title="Like me on Facebook" class="facebook">Facebook</a></li>
-      <li><a href="http://www.flickr.com/photos/haleyrobin" title="Follow me on Flickr" class="flickr">Flickr</a></li>
+      <li><a href="http://twitter.com/ha1eyrobin" title="Follow me on Twitter" class="twitter" target="_blank">Twitter</a></li>
+      <li><a href="http://www.facebook.com/photosbyheart" title="Like me on Facebook" class="facebook" target="_blank">Facebook</a></li>
+      <li><a href="http://www.flickr.com/photos/haleyrobin" title="Follow me on Flickr" class="flickr" target="_blank">Flickr</a></li>
     </ul>
   </nav>
   <div id="slider-wrapper" class="grid_11 alpha omega">
