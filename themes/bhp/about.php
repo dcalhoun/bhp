73393f03312f3e16c00838886e23ead2abc1431b
David Calhoun <dpcalhoun@gmail.com>
2011-04-20 06:58:30 -0500

    Added phpThumb to About page featured image.

--- themes/bhp/about.php
+++ themes/bhp/about.php
@@ -16,7 +16,16 @@ get_header(); ?>
 <?php if ( has_post_thumbnail() ) { ?>
   <div class="grid_6 alpha">
     <div class="featured-image">
-    <?php the_post_thumbnail(array(439,258), array('class' => 'thickborder')); ?>
+    <?php
+      //the_post_thumbnail(array(439,258), array('class' => 'thickborder'));
+      $image_id = get_post_thumbnail_id();
+      $featured_image = wp_get_attachment_url($image_id);
+      $width = 439;
+             
+      if ($featured_image) {
+        echo '<img src="' . get_bloginfo('template_directory') . '/phpthumb/phpThumb.php?src=' . $featured_image . '&w=' . $width . '&q=100&zc=1" class="thickborder" />';
+      }
+    ?>
     </div>
   </div>
   <div class="grid_5 omega">
